1a4555fa4da0c8e36d36857d1d27469dd0b42507
VinhHT2910 <vinhcute11023@gmail.com>
2025-06-03 14:57:58 +0700

    add search not found layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.ui.test.junit4)
     debugImplementation(libs.ui.tooling)
     debugImplementation(libs.ui.test.manifest)
+    implementation("com.google.android:flexbox:2.0.1")
 }
