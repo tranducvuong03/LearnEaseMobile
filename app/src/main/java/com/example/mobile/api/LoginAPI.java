dee077b0aecea7934357320eba6c23db96e2f81c
VinhHT2910 <vinhcute11023@gmail.com>
2025-06-29 20:47:10 +0700

    add chatbox AI

--- app/src/main/java/com/example/mobile/api/LoginAPI.java
+++ app/src/main/java/com/example/mobile/api/LoginAPI.java
@@ -49,7 +49,8 @@ public interface LoginAPI {
     @PUT("users/{id}/username")
     Call<Void> updateUserNameById(@Path("id") String userId,
                                   @Body UpdateUsernameRequest request);
-
+    @POST("ai/ask")
+    Call<String> askAI(@Body String userInput);
     @PUT("users/{id}/avatar")
     Call<Void> updateAvatarById(@Path("id") String userId,
                                   @Body UpdateAvatarRequest request);
